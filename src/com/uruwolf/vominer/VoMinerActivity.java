15059958dd050c843f30c285b607050f5d0deba8
Steve "Uru" West <uruwolf@gmail.com>
2012-05-05 19:46:38 +0100

    Added a nice shiny button and a toast that's triggered by it

--- src/com/uruwolf/vominer/VoMinerActivity.java
+++ src/com/uruwolf/vominer/VoMinerActivity.java
@@ -1,11 +1,13 @@
 package com.uruwolf.vominer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
 
-public class VoMinerActivity extends Activity {
+public class VoMinerActivity extends Activity implements OnClickListener{
 	
 	public static final String TAG = "Vo-Miner";
 	
@@ -14,6 +16,9 @@ public class VoMinerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button loadButton = (Button) findViewById(R.id.load_button);
+        loadButton.setOnClickListener(this);
     }
     
     @Override
@@ -22,4 +27,13 @@ public class VoMinerActivity extends Activity {
         inflater.inflate(R.menu.main_activity, menu);
         return true;
     }
+    
+    public void onClick(View v) {
+    	Context context = getApplicationContext();
+    	CharSequence text = "Hello toast!";
+    	int duration = Toast.LENGTH_SHORT;
+
+    	Toast toast = Toast.makeText(context, text, duration);
+    	toast.show();
+	}
 }
