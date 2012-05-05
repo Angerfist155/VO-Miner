b109b0933c0205bda80376747e22a1a96401297e
Steve "Uru" West <uruwolf@gmail.com>
2012-05-05 20:25:38 +0100

    Moved the string arrays into their own file for neatness. Added some commends to the actual code and created a little toast popup when the button is pressed

--- src/com/uruwolf/vominer/VoMinerActivity.java
+++ src/com/uruwolf/vominer/VoMinerActivity.java
@@ -7,8 +7,13 @@ import android.view.*;
 import android.view.View.*;
 import android.widget.*;
 
+/**
+ * The main activity for the app
+ * @author Uru
+ */
 public class VoMinerActivity extends Activity implements OnClickListener{
 	
+	//Tag to use for debugging
 	public static final String TAG = "Vo-Miner";
 	
     /** Called when the activity is first created. */
@@ -17,10 +22,14 @@ public class VoMinerActivity extends Activity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        //Make sure we can respond to button clicks
         Button loadButton = (Button) findViewById(R.id.load_button);
         loadButton.setOnClickListener(this);
     }
     
+    /**
+     * Show a fancy menu at the top right. Not sure if this will stay or not
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -28,12 +37,20 @@ public class VoMinerActivity extends Activity implements OnClickListener{
         return true;
     }
     
+    /**
+     * Process a click event for the button
+     */
     public void onClick(View v) {
-    	Context context = getApplicationContext();
-    	CharSequence text = "Hello toast!";
-    	int duration = Toast.LENGTH_SHORT;
 
-    	Toast toast = Toast.makeText(context, text, duration);
-    	toast.show();
+    	//Load up the three selected values from the spinners
+    	String system = (String) ((Spinner)findViewById(R.id.systemList)).getSelectedItem();
+    	String alpha = (String) ((Spinner)findViewById(R.id.gridAlphaList)).getSelectedItem();
+    	String num = (String) ((Spinner)findViewById(R.id.gridNumList)).getSelectedItem();
+    	
+    	//Show a Toast to make the user think that something happened
+    	Toast.makeText(getApplicationContext(),
+    			String.format(getString(R.string.toast_loading_message), system, alpha, num),
+    			Toast.LENGTH_SHORT)
+    			.show();
 	}
 }
--- src/com/uruwolf/vominer/fragments/ControlPanelFragment.java
+++ src/com/uruwolf/vominer/fragments/ControlPanelFragment.java
@@ -10,6 +10,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+/**
+ * Controls a view for selecting a system and sector and as well as a button to trigger loading information for said sector.
+ * @author Steve "Uru" West <uruwolf@gmail.com>
+ */
 public class ControlPanelFragment extends Fragment {
 	
 	@Override
@@ -19,20 +23,24 @@ public class ControlPanelFragment extends Fragment {
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		
+		//Set up the layout that we want to use for this fragment
 		View view = inflater.inflate(R.layout.sector_select, container, false);
 		
+		//Set up all the spinners. One for the system and another two for the coords. I hate how messy this is
 		Spinner systemSpinner = (Spinner) view.findViewById(R.id.systemList);
         ArrayAdapter<CharSequence> systemAdapter = ArrayAdapter.createFromResource(
                 getActivity(), R.array.system_list, android.R.layout.simple_spinner_item);
         systemAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         systemSpinner.setAdapter(systemAdapter);
 		
+        //Load the letter selection
         Spinner gridAlphaSpinner = (Spinner) view.findViewById(R.id.gridAlphaList);
         ArrayAdapter<CharSequence> gridAlphaAdapter = ArrayAdapter.createFromResource(
                 getActivity(), R.array.grid_alpha_list, android.R.layout.simple_spinner_item);
         gridAlphaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gridAlphaSpinner.setAdapter(gridAlphaAdapter);
-        
+
+        //And finally load the number selector
         Spinner gridNumSpinner = (Spinner) view.findViewById(R.id.gridNumList);
         ArrayAdapter<CharSequence> gridNumAdapter = ArrayAdapter.createFromResource(
                 getActivity(), R.array.grid_num_list, android.R.layout.simple_spinner_item);
