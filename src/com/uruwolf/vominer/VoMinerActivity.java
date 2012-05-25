91eeeff0a0f9dee57096679fa874ca82b78c8231
Steve "Uru" West <uruwolf@gmail.com>
2012-05-25 21:23:15 +0100

    Added license information

--- src/com/uruwolf/vominer/VoMinerActivity.java
+++ src/com/uruwolf/vominer/VoMinerActivity.java
@@ -1,3 +1,21 @@
+/*
+ * VoMinerActivity.java
+ * Copyright (C) 2011 Steve "Uru" West <uruwolf@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
 package com.uruwolf.vominer;
 
 import java.util.ArrayList;
@@ -93,12 +111,12 @@ public class VoMinerActivity extends Activity implements OnItemSelectedListener,
     /**
      * Show a fancy menu at the top right. Not sure if this will stay or not
      */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.main_activity, menu);
+//        return true;
+//    }
     
     @Override
     public void onPause(){
